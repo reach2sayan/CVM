added checks for stable numerical gradient and attemped fix to energy functions

src/energyfunctions.py
@@ -23,6 +23,18 @@ def F_efficient(corrs, mults_eci, multconfig_kb, all_vmat, vect_rhologrho, temp)
 
     return H + kB*temp*S
 
+def F_jacobian_efficient(corrs, mults_eci, multconfig_kb, all_vmat, vect_rhologrho, temp):
+
+    dH = mults_eci
+    dS = all_vmat.T @ (multconfig_kb * (1 + np.log(np.abs(all_vmat @ corrs))))
+
+    return dH + kB*temp*dS
+
+def F_hessian_efficient(corrs, mults_eci, multconfig_kb, all_vmat, vect_rhologrho, temp):
+
+    #d2S = ((multconfig_kb / (all_vmat @ corrs))[:, np.newaxis] * all_vmat).T @ all_vmat
+    d2S = (np.diag(multconfig_kb / (all_vmat @ corrs)).T @ all_vmat).T @ all_vmat
+    return kB*temp*d2S
 
 def F(corrs, vmat, kb, clusters, clustermult, configmult, T, eci):
     """
@@ -115,12 +127,6 @@ def F_jacobian(corrs, vmat, kb, clusters, clustermult, configmult, T, eci):
     return dH + kB*T*dS
 
 
-def F_jacobian_efficient(corrs, mults_eci, multconfig_kb, all_vmat, vect_rhologrho, temp):
-
-    dH = mults_eci
-    dS = all_vmat.T @ (multconfig_kb * (1 + np.log(np.abs(all_vmat @ corrs))))
-
-    return dH + kB*temp*dS
 
 
 def F_hessian(corrs, vmat, kb, clusters, clustermult, configmult, T, eci):
@@ -179,7 +185,3 @@ def F_hessian(corrs, vmat, kb, clusters, clustermult, configmult, T, eci):
 
     return kB*T*d2F
 
-def F_hessian_efficient(corrs, mults_eci, multconfig_kb, all_vmat, vect_rhologrho, temp):
-
-    d2S = ((multconfig_kb / (all_vmat @ corrs))[:, np.newaxis] * all_vmat).T @ all_vmat
-    return kB*temp*d2S

src/optimiser.py
@@ -142,7 +142,7 @@ def fit(F,
 
         if temp_results.fun < result_value:
             try:
-                assert not np.all(np.isnan(temp_results))
+                assert not np.all(np.isnan(temp_results.grad))
                 assert temp_results.status != 0
             except AssertionError:
                 print('Gradient blew up!! Incorrect solution. Moving on...')