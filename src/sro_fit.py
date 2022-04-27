d18e5ec7ab5cb8df12fe10e2e72ed56b1e851755
reach2sayan <sayan_samanta@brown.edu>
2022-04-26 21:48:51 -0400

    added sympy expression to func, finessed the fitting func

--- src/sro_fit.py
+++ src/sro_fit.py
@@ -3,6 +3,12 @@ from scipy.optimize import curve_fit
 import sys
 import json
 import inspect
+from sympy.parsing.sympy_parser import (
+    parse_expr,
+    standard_transformations,
+    implicit_multiplication,
+)
+from sympy import *
 
 eV2J = 96491.5666370759
 def sro_fit(func, results, coeff_in, len_struc, inJoules=False):
@@ -34,8 +40,7 @@ def sro_fit(func, results, coeff_in, len_struc, inJoules=False):
         print('File containing initial coefficients not found..taking defaults...')
         p0 = None
 
-    popt, pcov = curve_fit(f=func, xdata=xdata, ydata=ydata, p0=p0, maxfev=100000)
-
+    popt, pcov = curve_fit(f=func, xdata=xdata, ydata=ydata, p0=p0,method='dogbox', maxfev=10000000)
 
     for line in inspect.getsource(func).split('\n'):
         if 'return' in line:
@@ -44,12 +49,7 @@ def sro_fit(func, results, coeff_in, len_struc, inJoules=False):
     for index, varname in enumerate(func.__code__.co_varnames[1:]):
         return_line_replaced = return_line_replaced.replace(varname,f'{popt[index]}')
 
-    return_line_replaced = return_line_replaced.replace(' ','')
-    return_line_replaced = return_line_replaced.replace('++','+')
-    return_line_replaced = return_line_replaced.replace('+-','-')
-    return_line_replaced = return_line_replaced.replace('-+','-')
-    return_line_replaced = return_line_replaced.replace('--','+')
-    func_replace = {'np.exp':'EXP',
+    func_replace = {'np.exp':'exp',
                     'np.abs':'ABS',
                     'np.sin':'SIN'}
     for old_item, new_item in func_replace.items():
@@ -57,8 +57,17 @@ def sro_fit(func, results, coeff_in, len_struc, inJoules=False):
 
     xcont = np.linspace(min(xdata), max(xdata), 1000)[:, np.newaxis]
     ycont = func(xcont,*popt)
-    print(popt)
+    return_line_replaced = str(parse_expr(return_line_replaced,
+                                      transformations=standard_transformations+(implicit_multiplication,)).expand(basic=True))
+    return_line_replaced = return_line_replaced.replace('/T','*(T**(-1))')
+    return_line_replaced = return_line_replaced.replace(' ','')
+    return_line_replaced = return_line_replaced.replace('++','+')
+    return_line_replaced = return_line_replaced.replace('+-','-')
+    return_line_replaced = return_line_replaced.replace('-+','-')
+    return_line_replaced = return_line_replaced.replace('--','+')
+    return_line_replaced = return_line_replaced.replace('exp','EXP')
 
+    print(f'Output to func:\n{return_line_replaced}')
     np.savetxt('sro_fitting_calculated.out',np.hstack((xcont, ycont)))
     np.savetxt('sro_fitting_data.out',np.hstack((xdata[:,np.newaxis], ydata[:,np.newaxis])))
 
